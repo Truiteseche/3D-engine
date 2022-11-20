ff71f96a1a5f0eaa80147556218d2d140dcafc57
Truite Séchée <66438253+Truiteseche@users.noreply.github.com>
2022-11-20 22:17:53 +0000

    Fixed a bug that made it impossible to move an object on the Z axis

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ class Engine:
         pos : pos of the object
         points : list of tuples which are point's 3D coordinates relative to object's ones
         ex : [(0, 0, 0), (0, 0, 10), (0, 10, 0), (10, 0, 0)]
+        return the reference to the created object
         """
         self.objects.append(Object(pos, pointsCoordinates, self, color))
         return self.objects[len(self.objects)-1]
@@ -69,10 +70,10 @@ class Point:
     def projectPointOnScreen(self):
         """
         Calculates the projectedX and projectedY coordinates
-        It converts 3d coord to 2d coord on the screen
+        It converts 3d coord in the world to 2d coord on the screen
         """
-        self.projectedX = self.engine.SCREEN_DIST * (self.x + self.parentObject.x - self.engine.cameraX) / (self.engine.SCREEN_DIST + self.z - self.engine.cameraZ) + self.engine.SCREEN_SIZE[0]/2
-        self.projectedY = self.engine.SCREEN_DIST * (self.y + self.parentObject.y - self.engine.cameraY) / (self.engine.SCREEN_DIST + self.z - self.engine.cameraZ) + self.engine.SCREEN_SIZE[1]/2
+        self.projectedX = self.engine.SCREEN_DIST * (self.x + self.parentObject.x - self.engine.cameraX) / (self.engine.SCREEN_DIST + self.z + self.parentObject.z - self.engine.cameraZ) + self.engine.SCREEN_SIZE[0]/2
+        self.projectedY = self.engine.SCREEN_DIST * (self.y + self.parentObject.y - self.engine.cameraY) / (self.engine.SCREEN_DIST + self.z + self.parentObject.z - self.engine.cameraZ) + self.engine.SCREEN_SIZE[1]/2
 
     def drawPoint(self):
         self.projectPointOnScreen()
@@ -81,11 +82,12 @@ class Point:
 
 pygame.init()
 screen = pygame.display.set_mode((500, 480))
-pygame.display.set_caption('TRUITE ENGINE • 3D GRAPHICS')
+pygame.display.set_caption('TRUITE ENGINE • 3D GRAPHICS WIREFRAME')
 engine = Engine(screen)
-engine.create3dObject((0, 0, 0), [(0, 0, 0), (100, 0, 0), (100, 100, 0), (0, 100, 0), (0, 0, 100), (100, 0, 100), (100, 100, 100), (0, 100, 100)], (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-engine.create3dObject((0, 300, 0), [(0, 0, 0), (100, 0, 0), (100, 0, 100), (0, 0, 100), (50, -100, 50)], (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+engine.create3dObject((-50, 0, 0), [(0, 0, 0), (100, 0, 0), (100, 100, 0), (0, 100, 0), (0, 0, 100), (100, 0, 100), (100, 100, 100), (0, 100, 100)], (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+engine.create3dObject((0, 300, 50), [(0, 0, 0), (100, 0, 0), (100, 0, 100), (0, 0, 100), (50, -100, 50)], (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
 engine.create3dObject((300, 0, 0), [(25, 0, 0), (75, 0, 0), (100, 0, 25), (100, 0, 75), (75, 0, 100), (25, 0, 100), (0, 0, 75), (0, 0, 25), (25, 300, 0), (75, 300, 0), (100, 300, 25), (100, 300, 75), (75, 300, 100), (25, 300, 100), (0, 300, 75), (0, 300, 25)], (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+engine.create3dObject((200, 50, 150), [(0, 0, 0), (100, 0, 0), (100, 100, 0), (0, 100, 0), (0, 0, 100), (100, 0, 100), (100, 100, 100), (0, 100, 100)], (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
 
 while True:
     for event in pygame.event.get():
@@ -100,21 +102,21 @@ while True:
     
 
     if pygame.K_d in engine.keyboard and engine.keyboard[pygame.K_d] == True:
-        engine.cameraX += 0.3
+        engine.cameraX += 1
     if pygame.K_q in engine.keyboard and engine.keyboard[pygame.K_q] == True:
-        engine.cameraX -= 0.3
+        engine.cameraX -= 1
     if pygame.K_z in engine.keyboard and engine.keyboard[pygame.K_z] == True:
-        engine.cameraZ += 0.3
+        engine.cameraZ += 1
     if pygame.K_s in engine.keyboard and engine.keyboard[pygame.K_s] == True:
-        engine.cameraZ -= 0.3
+        engine.cameraZ -= 1
     if pygame.K_DOWN in engine.keyboard and engine.keyboard[pygame.K_DOWN] == True:
-        engine.cameraY += 0.3
+        engine.cameraY += 1
     if pygame.K_UP in engine.keyboard and engine.keyboard[pygame.K_UP] == True:
-        engine.cameraY -= 0.3
+        engine.cameraY -= 1
     if pygame.K_RIGHT in engine.keyboard and engine.keyboard[pygame.K_RIGHT] == True:
-        engine.SCREEN_DIST += 1
+        engine.SCREEN_DIST += 3
     if pygame.K_LEFT in engine.keyboard and engine.keyboard[pygame.K_LEFT] == True:
-        engine.SCREEN_DIST -= 1
+        engine.SCREEN_DIST -= 3
     
     #print(engine.cameraX, engine.cameraY)
     #print(engine.keyboard)
